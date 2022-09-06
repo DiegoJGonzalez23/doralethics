0c0d07d5d020a068100030063c47590389ef3c72
Diego Gonzalez <51039803+DiegoJGonzalez23@users.noreply.github.com>
2022-09-06 12:15:09 -0400

    d

--- includes/database.php
+++ includes/database.php
@@ -2,18 +2,18 @@
 
 // FOR WHEN UPLOADING TO WEBSITE
 
-//$dbHost = "localhost"; //  where to  host the stuff, right now its manual
-//$dbUser = "doraleth_diegog"; // normal
-//$dbPass = "Diegogamer#2005"; // xampp usually contains no database password
-//$dbName = "doraleth_ethicsdb"; // name of database
+$dbHost = "localhost"; //  where to  host the stuff, right now its manual
+$dbUser = "doraleth_diegog"; // normal
+$dbPass = "Diegogamer#2005"; // xampp usually contains no database password
+$dbName = "doraleth_ethicsdb"; // name of database
 
 
 
 // FOR WHEN MAKING EDITS ON THE LOCALHOST
-$dbHost = "localhost"; //  where to  host the stuff, right now its manual
-$dbUser = "root"; // normal
-$dbPass = ""; // xampp usually contains no database password
-$dbName = "doraleth_ethicsdb"; // name of database
+//$dbHost = "localhost"; //  where to  host the stuff, right now its manual
+//$dbUser = "root"; // normal
+//$dbPass = ""; // xampp usually contains no database password
+//$dbName = "doraleth_ethicsdb"; // name of database
 
 // my sql improved connect
 $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
